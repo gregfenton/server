09ccb9c7ac4ae6353457999ac3d3f1ab7d470ec0
Greg Fenton <greg.fenton@gmail.com>
2022-04-10 16:49:57 -0400

    add /slow endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/slow', (req, res, next) => {
+  console.log('delay for 3 seconds');
+  setTimeout(() => {
+    let secs = new Date().getTime();
+    res.send({ currentTime: secs });
+    console.log('end of 3 second delay');
+  }, 3000);
+});
+
 module.exports = router;
